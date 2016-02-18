9f2a0cedf68d5b0553a3dc1613692e0c06dc15aa
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2016-02-18 17:45:32 -0300

    Clipboard

--- src/text/Actions.java
+++ src/text/Actions.java
@@ -3,8 +3,20 @@ package text;
 public class Actions {
 
 	Notepad n;
-	
+
 	public Actions(Notepad n) {
 		this.n = n;
+	}
+
+	public void cut() {
+		n.getTextComponent().cut();
+	}
+	
+	public void copy() {
+		n.getTextComponent().copy();
+	}
+	
+	 public void paste(){
+	     n.getTextComponent().paste();
 	}
 }
